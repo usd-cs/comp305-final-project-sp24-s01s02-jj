0846f0a9901a979d9c3d5bde84c9228e63b6daf0
Andrew Albizati <andrew.albizati@gmail.com>
2024-04-29 15:24:04 -0700

    FEAT: Added a method in database to determine if a host is reachable on the network

--- src/main/java/edu/sandiego/comp305/sp24/schoolSim/Database.java
+++ src/main/java/edu/sandiego/comp305/sp24/schoolSim/Database.java
@@ -1,5 +1,6 @@
 package edu.sandiego.comp305.sp24.schoolSim;
 
+import java.net.InetAddress;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -24,9 +25,27 @@ public class Database {
         String url = "jdbc:mysql://" + Config.getInstance().getDatabaseHost() + ":3306/" + Config.getInstance().getDatabaseName();
 
         try {
+            if (!testHostConnection(Config.getInstance().getDatabaseHost())) {
+                throw new SQLException("Unable to connect to host!");
+            }
+
             this.databaseConnection = DriverManager.getConnection(url, Config.getInstance().getDatabaseUsername(), Config.getInstance().getDatabasePassword());
         } catch (SQLException e) {
             throw new IllegalStateException("Cannot connect the database!", e);
+        }
+    }
+
+    private boolean testHostConnection(String host) {
+        try {
+            InetAddress addr = InetAddress.getByName(host);
+            if (addr.isReachable(10)) {
+                return true;
+            } else {
+                return false;
+            }
+
+        } catch (Exception e) {
+            return false;
         }
     }
 
