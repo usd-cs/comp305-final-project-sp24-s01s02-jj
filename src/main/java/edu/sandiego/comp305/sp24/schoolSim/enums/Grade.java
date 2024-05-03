8b33be707d9e1fd5a32734f031010e518479310a
Andrew Albizati <andrew.albizati@gmail.com>
2024-05-03 11:00:51 -0700

    FEAT: Implemented the student class

--- src/main/java/edu/sandiego/comp305/sp24/schoolSim/enums/Grade.java
+++ src/main/java/edu/sandiego/comp305/sp24/schoolSim/enums/Grade.java
@@ -1,10 +1,29 @@
 package edu.sandiego.comp305.sp24.schoolSim.enums;
 
 public enum Grade{
-    FRESHMAN,
-    SOPHOMORE,
-    JUNIOR,
-    SENIOR,
-    GRADUATE,
-    DOCTORAL
+    FRESHMAN(1),
+    SOPHOMORE(2),
+    JUNIOR(3),
+    SENIOR(4),
+    GRADUATE(5),
+    DOCTORAL(6);
+
+    private final int id;
+
+    Grade(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public static Grade fromId(int id) {
+        for (Grade grade : Grade.values()) {
+            if (grade.getId() == id) {
+                return grade;
+            }
+        }
+        throw new IllegalArgumentException("Invalid Grade id: " + id);
+    }
 }
