8441d4890748c96fa25e0cd81e4abb159a9004ef
Andrew Albizati <andrew.albizati@gmail.com>
2024-05-04 17:45:28 -0700

    TEST: Implemented Building tests

--- src/test/java/edu/sandiego/comp305/sp24/schoolSim/model/BuildingTest.java
+++ src/test/java/edu/sandiego/comp305/sp24/schoolSim/model/BuildingTest.java
@@ -1,40 +1,90 @@
 package edu.sandiego.comp305.sp24.schoolSim.model;
 
+import edu.sandiego.comp305.sp24.schoolSim.Config;
+import edu.sandiego.comp305.sp24.schoolSim.Database;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class BuildingTest {
+    private static final int VALID_BUILDING_ID = 1;
+    private static final String VALID_BUILDING_NAME = "Building1";
+    private static final String VALID_BUILDING_ADDRESS = "1234 Building Ave";
+    private static final int VALID_BUILDING_FLOORS = 3;
+    private static final String VALID_BUILDING_ABBREVIATION = "BLD";
 
-    @Test
-    void addToDatabase() {
+    private static final String UNIQUE_BUILDING_NAME = "uniquename";
+    private static final String UNIQUE_BUILDING_ADDRESS = "uniqueaddr";
+    private static final int UNIQUE_BUILDING_FLOORS = 4;
+    private static final String UNIQUE_BUILDING_ABBREVIATION= "ABBR";
+
+
+    private static final int INVALID_BUILDING_ID = -1;
+    @BeforeAll
+    static void beforeAll() {
+        Config.initialize("config.properties");
     }
 
     @Test
-    void removeFromDatabase() {
+    void verifyFirstBuildingValues() {
+        Building building = new Building(VALID_BUILDING_ID);
+
+        assertEquals(VALID_BUILDING_ID, building.getId());
+        assertEquals(VALID_BUILDING_NAME, building.getName());
+        assertEquals(VALID_BUILDING_ADDRESS, building.getAddress());
+        assertEquals(VALID_BUILDING_FLOORS, building.getFloors());
+        assertEquals(VALID_BUILDING_ABBREVIATION, building.getAbbreviation());
     }
 
     @Test
-    void updateFromDatabase() {
+    void getBuildingInvalid() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            Building building = new Building(INVALID_BUILDING_ID);
+        });
     }
 
     @Test
-    void fetchFromDatabaseNotThere() {
+    void createNewBuildingDuplicate() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            Building building = new Building(UNIQUE_BUILDING_NAME, UNIQUE_BUILDING_ADDRESS, UNIQUE_BUILDING_FLOORS, VALID_BUILDING_ABBREVIATION);
+        });
     }
 
     @Test
-    void fetchFromDatabaseIsThere() {
+    void createNewBuildingAndTestVariables() {
+        Building building = new Building(
+                UNIQUE_BUILDING_NAME,
+                UNIQUE_BUILDING_ADDRESS,
+                UNIQUE_BUILDING_FLOORS,
+                UNIQUE_BUILDING_ABBREVIATION
+        );
+
+        assertEquals(UNIQUE_BUILDING_NAME, building.getName());
+        assertEquals(UNIQUE_BUILDING_ADDRESS, building.getAddress());
+        assertEquals(UNIQUE_BUILDING_FLOORS, building.getFloors());
+        assertEquals(UNIQUE_BUILDING_ABBREVIATION, building.getAbbreviation());
+
+        deleteWithId(building.getId(), "Building");
     }
 
-    @Test
-    void queryDatabaseNoResults() {
-    }
+    void deleteWithId(int id, String table) {
+        try {
+            String sql = "DELETE FROM " + table + " WHERE `id` = ?";
 
-    @Test
-    void queryDatabaseOneResult() {
-    }
+            PreparedStatement preparedStatement = Database.getInstance().getDatabaseConnection().prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+            preparedStatement.setLong(1, id);
 
-    @Test
-    void queryDatabaseManyResults() {
+            int affectedRows = preparedStatement.executeUpdate();
+            if (affectedRows == 0) {
+                throw new SQLException("User not deleted");
+            }
+        } catch (SQLException e) {
+            throw new IllegalStateException("User couldn't be deleted: " + e.getMessage());
+        }
     }
 }
