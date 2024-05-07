3b2646d72f07ee9004f43c4b90996b48b9120230
Noah Huang <nhuang@sandiego.edu>
2024-05-07 15:47:57 -0700

    Feat and Tests: Created tests stubs for Person Service, and SimplePerson class in order to make returned list easier to implement. Now methods do not need to worry about which type of person it queried. Started the first method of PersonService but has not been tested yet.

--- src/main/java/edu/sandiego/comp305/sp24/schoolSim/service/PersonService.java
+++ src/main/java/edu/sandiego/comp305/sp24/schoolSim/service/PersonService.java
@@ -1,14 +1,50 @@
 package edu.sandiego.comp305.sp24.schoolSim.service;
 
+import edu.sandiego.comp305.sp24.schoolSim.Database;
+import edu.sandiego.comp305.sp24.schoolSim.model.Department;
 import edu.sandiego.comp305.sp24.schoolSim.model.Person;
 
+import java.sql.*;
+
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
 public class PersonService {
-    public static List<Person> getAllWithFirstName(String firstName) {
-        return null;
+    public static List<Person> getAllWithFirstName(String firstName){
+        List<Person> resultList = new ArrayList<>();
+
+        ResultSet resultSet;
+        Connection connection = Database.getInstance().getDatabaseConnection();
+
+        String query = "SELECT * FROM Person WHERE first_name = ?";
+        try{
+            PreparedStatement preparedStatement = connection.prepareStatement(query);
+            preparedStatement.setString(1, firstName);
+            resultSet = preparedStatement.executeQuery();
+
+            while (resultSet.next()) {
+                String lastName = resultSet.getString("last_name");
+                Date birthdate = resultSet.getDate("birthday");
+                String phoneNumber = resultSet.getString("phone_number");
+                String username = resultSet.getString("username");
+                String organizationEmail = resultSet.getString("organization_email");
+                String secondaryEmail = resultSet.getString("secondary_email");
+                boolean isActive = resultSet.getBoolean("is_active");
+                Department department = new Department(resultSet.getInt("department"));
+
+                Person person = new SimplePerson(firstName, lastName, birthdate, phoneNumber, username, organizationEmail, secondaryEmail, isActive, department);
+                resultList.add(person);
+                }
+
+            } catch (SQLException e){
+                e.printStackTrace();
+        }
+
+        return resultList;
     }
+
+
     public static List<Person> getAllWithLastName(String lastName) {
         return null;
     }
---
+++ src/test/java/edu/sandiego/comp305/sp24/schoolSim/service/PersonServiceTest.java
@@ -0,0 +1,92 @@
+package edu.sandiego.comp305.sp24.schoolSim.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PersonServiceTest {
+
+    @Test
+    void getAllWithFirstNameReturnEmptyList() {
+    }
+
+    @Test
+    void getAllWithFirstNameReturnOneMatch() {
+    }
+
+    @Test
+    void getAllWithFirstNameReturnManyMatches() {
+    }
+
+    @Test
+    void getAllWithLastNameReturnEmptyList() {
+    }
+
+    @Test
+    void getAllWithLastNameReturnOneMatch() {
+    }
+
+    @Test
+    void getAllWithLastNameReturnManyMatch() {
+    }
+
+    @Test
+    void getAllWithPhoneNumberReturnNoMatch() {
+    }
+
+    @Test
+    void getAllWithPhoneNumberReturnOneMatch() {
+    }
+
+    @Test
+    void getAllWithPhoneNumberReturnManyMatch() {
+    }
+
+    @Test
+    void getByUsernameReturnNoMatch() {
+    }
+
+    @Test
+    void getByUsernameReturnMatch() {
+    }
+
+    @Test
+    void getByOrganizationEmailReturnNoMatch() {
+    }
+
+    @Test
+    void getByOrganizationEmailReturnMatch() {
+    }
+
+    @Test
+    void getBySecondaryEmailReturnNoMatch() {
+    }
+
+    @Test
+    void getBySecondaryEmailReturnMatch() {
+    }
+
+    @Test
+    void getActivePeopleReturnNoMatch() {
+    }
+
+    @Test
+    void getActivePeopleReturnOneMatch() {
+    }
+
+    @Test
+    void getActivePeopleReturnManyMatch() {
+    }
+
+    @Test
+    void getInactivePeopleReturnNoMatch() {
+    }
+    @Test
+    void getInactivePeopleReturnOneMatch() {
+    }
+
+    @Test
+    void getInactivePeopleReturnManyMatch() {
+    }
+
+}
