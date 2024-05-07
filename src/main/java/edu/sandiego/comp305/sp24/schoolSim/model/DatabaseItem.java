0744b6cfe8ba49cc6a693d4ed756e0450822f73c
John Phillips <johnphillips@sandiego.edu>
2024-05-07 15:41:28 -0700

    feat: database interfaces

--- src/main/java/edu/sandiego/comp305/sp24/schoolSim/model/DatabaseItem.java
+++ src/main/java/edu/sandiego/comp305/sp24/schoolSim/model/DatabaseItem.java
@@ -1,8 +1,24 @@
 package edu.sandiego.comp305.sp24.schoolSim.model;
 
 import java.util.List;
+import java.util.Map;
 
 public interface DatabaseItem {
+
+    /**
+     * Get an instance of an object representing the table containing this item. Used
+     * to grab column names needed to properly make a string map.
+     *
+     * @return An instance of the table containing this item.
+     */
     DatabaseTable getParentTable();
-    List<String> getStringList();
+
+    /**
+     * Get mappings of column names to object values. The number of keys should match
+     * getParentTable().getColumnNames().size(). There should be one mapping for every
+     * entry in that same list to a string representation of the matching attribute.
+     *
+     * @return A mapping of this item's elements as strings to their column in the table
+     */
+    Map<String, String> getStringMap();
 }
--- src/main/java/edu/sandiego/comp305/sp24/schoolSim/model/DatabaseTable.java
+++ src/main/java/edu/sandiego/comp305/sp24/schoolSim/model/DatabaseTable.java
@@ -3,8 +3,26 @@ package edu.sandiego.comp305.sp24.schoolSim.model;
 import java.util.List;
 
 public interface DatabaseTable {
+
+    /**
+     * Return the name of this table.
+     *
+     * @return The name of this table in the database as a string.
+     */
     String getTableName();
+
+    /**
+     * Get the number of rows in this table.
+     *
+     * @return Number of rows in this table.
+     */
     long getCountTableRows();
+
+    /**
+     * Get a list of the columns in this table.
+     *
+     * @return A list of the columns in this table as a string.
+     */
     List<String> getColumnNames();
 
 }
