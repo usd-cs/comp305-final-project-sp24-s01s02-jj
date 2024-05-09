b6a5e090664ccb272298b0c0cf5fcbbcc938c445
Noah Huang <nhuang@sandiego.edu>
2024-05-09 14:56:30 -0700

    committing the simplePerson class

---
+++ src/main/java/edu/sandiego/comp305/sp24/schoolSim/service/SimplePerson.java
@@ -0,0 +1,16 @@
+package edu.sandiego.comp305.sp24.schoolSim.service;
+
+import edu.sandiego.comp305.sp24.schoolSim.model.Department;
+import edu.sandiego.comp305.sp24.schoolSim.model.Person;
+
+import java.sql.Date;
+
+public class SimplePerson extends Person {
+    SimplePerson(String firstName, String lastName, Date birthdate, String phoneNumber, String username,
+                 String organizationEmail, String secondaryEmail, boolean isActive, Department department)
+    {
+        super(firstName, lastName, birthdate, phoneNumber, username,
+                organizationEmail, secondaryEmail, isActive, department);
+
+    }
+}
