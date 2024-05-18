e802bd17a2bedffcfc8e51f5f7790bb31af43be2
Andrew Albizati <andrew.albizati@gmail.com>
2024-05-18 12:17:57 -0700

    REFACTOR: Removed magic numbers and variables from AlumniTest

--- src/test/java/edu/sandiego/comp305/sp24/schoolSim/model/AlumniTest.java
+++ src/test/java/edu/sandiego/comp305/sp24/schoolSim/model/AlumniTest.java
@@ -10,12 +10,15 @@ import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class AlumniTest {
+    private static final String CONFIG_FILENAME = "config.properties";
+    private static final int SQL_DATE_OFFSET = 1900;
+    private static final int ALUMNI_ADDITIONAL_PARAMETER_COUNT = 2;
+
     private static final int VALID_ALUMNI_ID = 1;
     private static final String VALID_ALUMNI_USERNAME = "alumni";
     private static final String VALID_ALUMNI_GRADUATION_DATE = "2023-07-31";
@@ -25,18 +28,22 @@ class AlumniTest {
 
     private static final String FAKE_FIRST_NAME = "FAKENAME";
     private static final String FAKE_LAST_NAME = "FAKELAST";
-    private static final Date FAKE_BIRTHDATE = new Date(2004 - 1900, 4, 18);
+    private static final Date FAKE_BIRTHDATE = new Date(2004 - SQL_DATE_OFFSET, 4, 18);
     private static final String FAKE_PHONE_NUMBER = "8584940009";
     private static final String UNIQUE_USERNAME = "uniquealum";
     private static final String UNIQUE_ORG_EMAIl = "uniquealum@sandiego.edu";
     private static final String UNIQUE_SECONDARY_EMAIL = "uniquealum@gmail.com";
     private static final int FAKE_DEPARTMENT_ID = 1;
-    private static final Date FAKE_ALUMNI_GRADUATION_DATE = new Date(2024 - 1900, 6, 30);
+    private static final Date FAKE_ALUMNI_GRADUATION_DATE = new Date(2024 - SQL_DATE_OFFSET, 6, 30);
     private static final DegreeType FAKE_ALUMNI_DEGREE = DegreeType.BACHELOR;
+
+    private static final String PERSON_TABLE_NAME = "Person";
+    private static final String ALUMNI_TABLE_NAME = "Alumni";
+
 
     @BeforeAll
     static void beforeAll() {
-        Config.initialize("config.properties");
+        Config.initialize(CONFIG_FILENAME);
     }
     @Test
     void verifyFirstAlumniValues() {
@@ -92,8 +99,8 @@ class AlumniTest {
 
 
         // Delete person so test can be re-run
-        deleteWithId(alumni.getId(), "Person");
-        deleteWithId(alumni.getId(), "Alumni");
+        deleteWithId(alumni.getId(), PERSON_TABLE_NAME);
+        deleteWithId(alumni.getId(), ALUMNI_TABLE_NAME);
     }
 
     void deleteWithId(long id, String table) {
@@ -117,8 +124,9 @@ class AlumniTest {
         Alumni valid = new Alumni(VALID_ALUMNI_ID);
 
         List<String> actual = valid.getStringList();
-        int alumniSpecificStart = actual.size()-2;
+        int alumniSpecificStart = actual.size() - ALUMNI_ADDITIONAL_PARAMETER_COUNT;
+
         assertEquals(valid.getGraduationDate().toString(), actual.get(alumniSpecificStart));
-        assertEquals(valid.getDegree().toString(), actual.get(alumniSpecificStart+1));
+        assertEquals(valid.getDegree().toString(), actual.get(++alumniSpecificStart));
     }
 }
