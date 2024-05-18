dd841e2a5d2f70c90b79ae8a0d43ee52ec932e60
Andrew Albizati <andrew.albizati@gmail.com>
2024-05-18 12:30:07 -0700

    REFACTOR: Removed magic numbers and variables from EmployeeTest

--- src/test/java/edu/sandiego/comp305/sp24/schoolSim/model/EmployeeTest.java
+++ src/test/java/edu/sandiego/comp305/sp24/schoolSim/model/EmployeeTest.java
@@ -4,19 +4,25 @@ import edu.sandiego.comp305.sp24.schoolSim.Config;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+import java.sql.Date;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class EmployeeTest {
-    private static final String CONFIG = "config.properties";
+    private static final String CONFIG_FILENAME = "config.properties";
+    private static final int SQL_DATE_OFFSET = 1900;
+    private static final int EMPLOYEE_SPECIFIC_VARIABLE_COUNT = 3;
+    private static final String NO_MANAGER_VALUE = "No Manager";
+
     private static final int VALID_EMPLOYEE_ID_NO_MANAGER = 5;
+    private static final Date VALID_EMPLOYEE_START_DATE = new Date(2024 - SQL_DATE_OFFSET, 4, 4);
     private static final double VALID_EMPLOYEE_WAGE_NO_MANAGER = 16.85;
     private static final int VALID_EMPLOYEE_ID_WITH_MANAGER = 6;
 
     @BeforeAll
     static void beforeAll() {
-        Config.initialize(CONFIG);
+        Config.initialize(CONFIG_FILENAME);
     }
 
     @Test
@@ -34,7 +40,7 @@ class EmployeeTest {
         Employee employee = new Employee(VALID_EMPLOYEE_ID_NO_MANAGER);
         assertEquals(VALID_EMPLOYEE_ID_NO_MANAGER, employee.getId());
         assertEquals(VALID_EMPLOYEE_WAGE_NO_MANAGER, employee.getHourlyWage());
-        assertEquals("2024-05-04", employee.getStartDate().toString());
+        assertEquals(VALID_EMPLOYEE_START_DATE, employee.getStartDate());
         assertTrue(employee.getManager().isEmpty());
     }
 
@@ -50,27 +56,28 @@ class EmployeeTest {
         Employee valid = new Employee(VALID_EMPLOYEE_ID_NO_MANAGER);
 
         List<String> actual = valid.getStringList();
-        int employeeSpecificStart = actual.size()-3;
-        assertEquals("No Manager", actual.get(employeeSpecificStart));
-        assertEquals(valid.getStartDate().toString(),actual.get(employeeSpecificStart+1));
-        assertEquals("$"+Double.toString(valid.getHourlyWage()), actual.get(employeeSpecificStart+2));
+        int employeeSpecificStart = actual.size() - EMPLOYEE_SPECIFIC_VARIABLE_COUNT;
 
+        assertEquals(NO_MANAGER_VALUE, actual.get(employeeSpecificStart));
+        assertEquals(valid.getStartDate().toString(),actual.get(++employeeSpecificStart));
+        assertEquals("$" + valid.getHourlyWage(), actual.get(++employeeSpecificStart));
     }
 
     @Test
     void getStringListValuesManager() {
         Employee valid = new Employee(VALID_EMPLOYEE_ID_WITH_MANAGER);
-        // Always true; If statement to compile
+
         Employee manager = null;
         if (valid.getManager().isPresent()) {
             manager = valid.getManager().get();
         }
+
         List<String> actual = valid.getStringList();
-        int employeeSpecificStart = actual.size()-3;
+        int employeeSpecificStart = actual.size() - EMPLOYEE_SPECIFIC_VARIABLE_COUNT;
+
         assertNotNull(manager);
         assertEquals(manager.toString(), actual.get(employeeSpecificStart));
-        assertEquals(valid.getStartDate().toString(),actual.get(employeeSpecificStart+1));
-        assertEquals("$"+Double.toString(valid.getHourlyWage()), actual.get(employeeSpecificStart+2));
-
+        assertEquals(valid.getStartDate().toString(),actual.get(++employeeSpecificStart));
+        assertEquals("$" + valid.getHourlyWage(), actual.get(++employeeSpecificStart));
     }
 }
