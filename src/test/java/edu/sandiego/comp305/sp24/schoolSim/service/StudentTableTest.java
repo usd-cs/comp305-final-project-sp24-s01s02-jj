b9c82e97e85ff1f7d019ba7ca3bfb9c65e2c2b2c
Noah Huang <nhuang@sandiego.edu>
2024-05-18 22:57:31 -0700

    Test and Feat: Finished implementing and testing StudentTable. Implementation/Usage is like all the the other tables. All test passed

--- src/test/java/edu/sandiego/comp305/sp24/schoolSim/service/StudentTableTest.java
+++ src/test/java/edu/sandiego/comp305/sp24/schoolSim/service/StudentTableTest.java
@@ -1,6 +1,7 @@
 package edu.sandiego.comp305.sp24.schoolSim.service;
 
 import edu.sandiego.comp305.sp24.schoolSim.Config;
+import edu.sandiego.comp305.sp24.schoolSim.enums.Grade;
 import edu.sandiego.comp305.sp24.schoolSim.model.Person;
 import edu.sandiego.comp305.sp24.schoolSim.model.Student;
 import org.junit.jupiter.api.BeforeAll;
@@ -16,6 +17,12 @@ class StudentTableTest {
     ArrayList<Person> twoPersonList = new ArrayList<>();
     ArrayList<Person> emptyList = new ArrayList<>();
 
+    Student fakeStudent = new Student(55);
+
+    Student myHomieKevin = new Student(374);
+
+    Student myHomieRonnie = new Student(375);
+
 
     @BeforeAll
     static void beforeAll() {
@@ -28,18 +35,48 @@ class StudentTableTest {
         List<Student> returnList = table.getAllWithMajor("UngaBunga");
         assertEquals(returnList, emptyList);
     }
-
+    @Test
     void getAllWithMajorOneMatch() {
         StudentTable table = new StudentTable();
-        List<Student> returnList = table.getAllWithMajor("Computer Science");
+        List<Student> returnedList = table.getAllWithMajor("Computer Science");
+        singlePersonList.add(fakeStudent);
+        assertEquals(singlePersonList.size(), returnedList.size());
+        assertEquals(singlePersonList.get(0).getId(),returnedList.get(0).getId());
     }
-
+    @Test
     void getAllWithMajorTwoMatches() {
         StudentTable table = new StudentTable();
         List<Student> returnList = table.getAllWithMajor("Math");
+        twoPersonList.add(myHomieKevin);
+        twoPersonList.add(myHomieRonnie);
+        assertEquals(returnList.size(), twoPersonList.size());
+        assertEquals(twoPersonList.get(0).getId(),returnList.get(0).getId());
+        assertEquals(twoPersonList.get(1).getId(),returnList.get(1).getId());
     }
 
     @Test
-    void getAllInGrade() {
+    void getAllInGradeNoMatches() {
+        StudentTable table = new StudentTable();
+        List<Student> returnedList = table.getAllInGrade("1");
+        assertEquals(returnedList, emptyList);
+    }
+    @Test
+    void getAllInGradeOneMatch() {
+        StudentTable table = new StudentTable();
+        List<Student> returnedList = table.getAllInGrade("2");
+        singlePersonList.add(fakeStudent);
+        assertEquals(singlePersonList.size(), returnedList.size());
+        assertEquals(singlePersonList.get(0).getId(),returnedList.get(0).getId());
+    }
+    @Test
+    void getAllInGradeTwoMatch() {
+        StudentTable table = new StudentTable();
+        List<Student> returnedList = table.getAllInGrade("4");
+        twoPersonList.add(myHomieKevin);
+        twoPersonList.add(myHomieRonnie);
+        assertEquals(returnedList.size(), twoPersonList.size());
+        assertEquals(twoPersonList.get(0).getId(),returnedList.get(0).getId());
+        assertEquals(twoPersonList.get(1).getId(),returnedList.get(1).getId());
+
     }
 }
