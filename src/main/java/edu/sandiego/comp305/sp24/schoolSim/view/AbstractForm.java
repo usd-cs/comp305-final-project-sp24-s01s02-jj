cd8470a35976ca5537e31f4c2309dd001db5d88f
John Phillips <johnphillips@sandiego.edu>
2024-05-19 20:43:50 -0700

    test: refactor tests for setter instead of getter

--- src/main/java/edu/sandiego/comp305/sp24/schoolSim/view/AbstractForm.java
+++
@@ -1,9 +0,0 @@
-package edu.sandiego.comp305.sp24.schoolSim.view;
-
-public abstract class AbstractForm {
-    public abstract String toBulmaForm(String postPath);
-
-    public String addFooterButtons(String path) {
-        return "";
-    }
-}
--- src/test/java/edu/sandiego/comp305/sp24/schoolSim/view/PersonFormTest.java
+++ src/test/java/edu/sandiego/comp305/sp24/schoolSim/view/PersonFormTest.java
@@ -8,6 +8,7 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import java.sql.Date;
+import java.sql.SQLException;
 
 class PersonFormTest {
     @BeforeAll
@@ -16,38 +17,15 @@ class PersonFormTest {
     }
 
     @Test
-    void toBulmaForm() {
+    void setDepartmentNonExistent() {
+        PersonForm form = new PersonForm();
+        assertThrows(IllegalArgumentException.class, () -> {form.setDepartment("Not a real department");});
     }
 
     @Test
-    void getDepartmentNonExistent() {
-        PersonForm form = new PersonForm(
-                "Alex",
-                "Mason",
-                new Date(1966-1900,7, 24),
-                "1234567890",
-                "amason",
-                "amason@acme.org",
-                "alex@amason.io",
-                true,
-                "Not A Real Department"
-        );
-        assertNull(form.getDepartment());
-    }
-
-    @Test
-    void getDepartmentValid() {
-        PersonForm form = new PersonForm(
-                "Alex",
-                "Mason",
-                new Date(1966-1900,7, 24),
-                "1234567890",
-                "amason",
-                "amason@acme.org",
-                "alex@amason.io",
-                true,
-                "Fake Department 1"
-        );
+    void setDepartmentValid() {
+        PersonForm form = new PersonForm();
+        assertDoesNotThrow(()->{form.setDepartment("Fake Department 1");});
         assertEquals(1, form.getDepartment().getId());
     }
 
