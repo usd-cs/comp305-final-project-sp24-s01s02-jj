2ba84c6833560b6d8ff508f92f2208f2ccd131a9
Andrew Albizati <andrew.albizati@gmail.com>
2024-05-20 00:03:11 -0700

    TEST: Added tests for deleteFromDatabase method for all tables

--- src/test/java/edu/sandiego/comp305/sp24/schoolSim/service/DepartmentTableTest.java
+++ src/test/java/edu/sandiego/comp305/sp24/schoolSim/service/DepartmentTableTest.java
@@ -1,10 +1,13 @@
 package edu.sandiego.comp305.sp24.schoolSim.service;
 
 import edu.sandiego.comp305.sp24.schoolSim.Config;
+import edu.sandiego.comp305.sp24.schoolSim.enums.DegreeType;
+import edu.sandiego.comp305.sp24.schoolSim.model.Alumni;
 import edu.sandiego.comp305.sp24.schoolSim.model.Department;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+import java.sql.Date;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -26,5 +29,27 @@ class DepartmentTableTest {
         DepartmentTable table = new DepartmentTable();
         long results = table.getByName("Fake Department 1").get().getId();
         assertEquals(fakeDepartment.getId(), results);
+    }
+
+    @Test
+    void deleteFromDatabaseExists() {
+        DepartmentTable departmentTable = new DepartmentTable();
+
+        Department department = new Department("Fake department name");
+
+        departmentTable.deleteFromDatabase(department.getId());
+
+        assertThrows(IllegalArgumentException.class, () -> {
+            Department department1 = new Department(department.getId());
+        });
+    }
+
+    @Test
+    void deleteFromDatabaseDoesntExist() {
+        DepartmentTable departmentTable = new DepartmentTable();
+
+        assertThrows(IllegalStateException.class, () -> {
+            departmentTable.deleteFromDatabase(-1);
+        });
     }
 }
