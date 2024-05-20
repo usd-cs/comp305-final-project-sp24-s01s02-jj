2cf62b6d59d39cd0da82a69052751e52546a5831
John Phillips <johnphillips@sandiego.edu>
2024-05-19 17:13:18 -0700

    [wip]feat: add modal form

--- src/main/java/edu/sandiego/comp305/sp24/schoolSim/view/PersonForm.java
+++ src/main/java/edu/sandiego/comp305/sp24/schoolSim/view/PersonForm.java
@@ -1,11 +1,41 @@
 package edu.sandiego.comp305.sp24.schoolSim.view;
 
+import edu.sandiego.comp305.sp24.schoolSim.Database;
+import edu.sandiego.comp305.sp24.schoolSim.model.DatabaseTable;
 import edu.sandiego.comp305.sp24.schoolSim.model.Department;
+import edu.sandiego.comp305.sp24.schoolSim.model.Employee;
+import edu.sandiego.comp305.sp24.schoolSim.model.Person;
 
-import java.sql.Date;
+import jakarta.validation.constraints.*;
 
-public class PersonForm {
-    public Person(String firstName,
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class PersonForm extends AbstractForm {
+
+    @NotNull
+    private String firstName;
+    @NotNull
+    private String lastName;
+    @Past
+    @NotNull
+    private Date birthDate;
+    @NotNull
+    private String phoneNumber;
+    @NotNull
+    private String username;
+    @NotNull
+    @Email
+    private String organizationEmail;
+    @Email
+    private String secondaryEmail;
+    @NotNull
+    private boolean isActive;
+    @NotNull
+    private String department;
+
+    public PersonForm(String firstName,
                   String lastName,
                   Date birthdate,
                   String phoneNumber,
@@ -13,6 +43,88 @@ public class PersonForm {
                   String organizationEmail,
                   String secondaryEmail,
                   boolean isActive,
-                  Department department) {
+                  String department) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.birthDate = birthdate;
+        this.phoneNumber = phoneNumber;
+        this.username = username;
+        this.organizationEmail = organizationEmail;
+        this.secondaryEmail = secondaryEmail;
+        this.isActive = isActive;
+        this.department = department;
+    }
+
+    public void build() {
+        new Person(getFirstName(),
+                getLastName(),
+                getBirthDate(),
+                getPhoneNumber(),
+                getUsername(),
+                getOrganizationEmail(),
+                getSecondaryEmail(),
+                isActive(),
+                getDepartment());
+    }
+
+    @Override
+    public String toBulmaForm(String postPath) {
+        return "";
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public Date getBirthDate() {
+        return birthDate;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getOrganizationEmail() {
+        return organizationEmail;
+    }
+
+    public String getSecondaryEmail() {
+        return secondaryEmail;
+    }
+
+    public boolean isActive() {
+        return isActive;
+    }
+
+    public Department getDepartment() {
+        // Uncomment once departmentTable exists
+        // DatabaseTable departmentTable = new DepartmentTable();
+        String query = String.format("SELECT id FROM %s WHERE name = ?", "Department");
+        ResultSet resultSet;
+        Connection connection = Database.getInstance().getDatabaseConnection();
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement(query);
+            preparedStatement.setString(1, this.department);
+            resultSet = preparedStatement.executeQuery();
+
+            if (resultSet.next()) {
+                long id = resultSet.getLong("id");
+                return new Department(id);
+            } else {
+                return null;
+            }
+
+        } catch (SQLException e){
+            e.printStackTrace();
+        }
+        return null;
     }
 }
